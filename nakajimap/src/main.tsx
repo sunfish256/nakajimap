f867b957e417f80c8770c5bd76304badcc3cf47f
Yanbo Ma <reg.n.cal@gmail.com>
2024-06-19 20:24:43 +0900

    マップAPI実装、ホーム画面のスタイリング

--- nakajimap/src/main.tsx
+++ nakajimap/src/main.tsx
@@ -1,10 +1,29 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App.tsx"
-import "./index.css"
+import App from "./App"
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const container = document.getElementById("root")
+if (container) {
+  const root = ReactDOM.createRoot(container)
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+} else {
+  console.error("Root container not found")
+}
+
+// Google Maps APIを非同期で読み込むスクリプトを動的に追加
+function loadGoogleMapsApi(): void {
+  const script = document.createElement("script")
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${
+    import.meta.env.VITE_GOOGLEMAP_API_KEY
+  }&callback=initMap&libraries=places`
+  script.async = true
+  script.defer = true
+  document.head.appendChild(script)
+}
+
+// スクリプトのロードを開始
+loadGoogleMapsApi()
