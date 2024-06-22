9d7b938d1b892cabc6acda54f7f5f71ef8a94f9b
knakajima <kota.nakajima@ibm.com>
2024-06-22 12:11:02 +0900

    ディレクトリ構成の変更

--- nakajimap/src/App.tsx
+++ nakajimap/src/App.tsx
@@ -2,13 +2,13 @@ import React from "react"
 import Home from "./pages/Home"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import { Auth } from "./pages/Auth"
-import "./App.css"
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/auth" element={<Auth />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/" element={<Auth />} /> {/* デフォルトはAuthに */}
       </Routes>
     </Router>
--- nakajimap/src/pages/Home.tsx
+++ nakajimap/src/pages/Home.tsx
@@ -1,24 +1,36 @@
 import Filtering from '../components/Filtering'
 import Map from '../components/Map'
 import SearchResult from '../components/Table'
+import "../Home.css"
 
 const Home: React.FC = () => {
-    return (
+  return (
+    <div>
+      <div className="sticky-header">
+        <h1>ここにロゴ・サイト名・ハンバーガーを配置する</h1>
+      </div>
       <div className="container">
         <div className="content">
-          <Filtering />
+          <div className="filter">
+            <Filtering />
+          </div>
         </div>
         <div className="content">
-          <div className="search-result">
+          <div className="result-title">
             <h2>検索結果</h2>
-            <SearchResult />
           </div>
-          <div className="map">
-            <Map />
+          <div className="result-items">
+            <div className="result-table">
+              <SearchResult />
+            </div>
+            <div className="result-map">
+              <Map />
+            </div>
           </div>
         </div>
       </div>
-    )
+    </div>
+  )
   }
 
 export default Home
