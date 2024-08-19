2eb2dff15d06f630d4b4818f0de9a6790090e894
knakajima <kotanakajima@hotmail.com>
2024-08-19 22:16:49 +0900

    レイアウト修正

--- nakajimap/src/components/AuthLayout.tsx
+++ nakajimap/src/components/AuthLayout.tsx
@@ -1,17 +1,28 @@
 import React from "react"
 // import Navbar from "./Navbar"
-import { AppBar, Toolbar } from "@mui/material"
-import logo from "../assets/nakajimap_logo.png"
+import { AppBar, Toolbar, Box, Typography } from "@mui/material"
 
 const AuthLayout: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
   return (
     <div>
       <AppBar position="static">
         <Toolbar>
-          <img src={logo} alt="ロゴ" style={{ height: 40, marginRight: "auto" }} />
+          <Box sx={{ flexGrow: 1 }}>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ 
+                color: 'white', 
+                fontSize: '1.5rem',
+                letterSpacing: '0.2rem',
+                fontWeight: 500,
+              }}
+            >
+              nakajimap
+            </Typography>
+          </Box>
         </Toolbar>
       </AppBar>
-      {/* <Navbar /> */}
       <main>{children}</main>
     </div>
   )
